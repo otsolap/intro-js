6db2ddc3b7a5a1d2a6f5ed79ddc75ddb68e86b46
otsolap <59653348+OtsoL@users.noreply.github.com>
2020-01-30 14:56:30 +0200

    Jquery added
    changed some javascript into jquery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,54 @@
-(function () {
-    var itemClassName = 'slider__photo';
-        items = document.getElementsByClassName(itemClassName),
-        totalItems = items.length,
-        slide = 0;
+(function() {
+  var itemClassName = "slider__photo";
+  (items = document.getElementsByClassName(itemClassName)),
+    (totalItems = items.length),
+    (slide = 0);
 
-    // Set event listeners
-    function setEventListeners() {
-        var next = document.getElementsByClassName('slider__button--next')[0],
-            prev = document.getElementsByClassName('slider__button--prev')[0];
+  // Set event listeners
+  function setEventListeners() {
+    var next = document.getElementsByClassName("slider__button--next")[0],
+      prev = document.getElementsByClassName("slider__button--prev")[0];
 
-        next.addEventListener('click', getNext);
-        prev.addEventListener('click', getPrev);
+    next.addEventListener("click", getNext);
+    prev.addEventListener("click", getPrev);
+  }
+
+  function getNext() {
+    $(".slider_photo")
+      .eq(slide)
+      .removeClass("active");
+    $(".slider_photo")
+      .eq(0)
+      .removeClass("active");
+
+    /* Plain javascript
+    document.getElementsByClassName('slider__photo')[slide].classList.remove('active');
+    slide = (slide === totalItems - 1) ? 0 : (slide + 1);        document.getElementsByClassName('slider__photo')[slide].classList.add('active');
+         */
+  }
+
+  function getPrev() {
+    document
+      .getElementsByClassName("slider__photo")
+      [slide].classList.remove("active");
+
+    if (slide === 0) {
+      slide = document.getElementsByClassName("slider__photo").length - 1;
+    } else {
+      slide--;
     }
+    document
+      .getElementsByClassName("slider__photo")
+      [slide].classList.add("active");
+  }
 
-    function getNext() {
-        document.getElementsByClassName('slider__photo')[slide].classList.remove('active');
+  function initSlider() {
+    // setInitialClasses();
+    setEventListeners();
 
-        slide = (slide === totalItems - 1) ? 0 : (slide + 1);
+    // Set moving to false so that the slider becomes interactive
+    moving = false;
+  }
 
-        document.getElementsByClassName('slider__photo')[slide].classList.add('active');
-    }
-
-    function getPrev(){
-        document.getElementsByClassName('slider__photo')[slide].classList.remove('active');
-
-        if (slide === 0) {
-            slide = document.getElementsByClassName('slider__photo').length - 1;
-        } else {
-            slide--;
-        }
-        document.getElementsByClassName('slider__photo')[slide].classList.add('active');
-    }
-
-    function initSlider() {
-        // setInitialClasses();
-        setEventListeners();
-        
-        // Set moving to false so that the slider becomes interactive
-        moving = false;
-    }
-
-    initSlider();
+  initSlider();
 })();
